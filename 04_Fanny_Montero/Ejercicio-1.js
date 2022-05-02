f4316c5476b0e8b03a0e09ef87316ddd6e7295c9
Yaneth Montero <MonteroCristobal@outlook.es>
2022-05-01 19:22:56 -0500

    probé JavaScript

---
+++ 04_Fanny_Montero/Ejercicio-1.js
@@ -0,0 +1,22 @@
+var mensj= prompt("Hiciste tu tarea? ")
+    if (mensj==="si"){
+        alert("¡Bien hecho!");
+    }
+    else if(mensj==="SI"){
+        alert("¡Bien hecho!");
+    }
+    else if(mensj==="Si"){
+        alert("¡Bien hecho!");
+    }
+    else if(mensj==="no"){
+        alert("No te rindas, ¡empieza ya mismo!");
+    }
+    else if(mensj==="NO"){
+        alert("No te rindas, ¡empieza ya mismo!");
+    }
+    else if(mensj==="No"){
+        alert("No te rindas, ¡empieza ya mismo!");
+    }
+    else{
+        alert("Error, no pusiste una respuesta correcta");
+    }
